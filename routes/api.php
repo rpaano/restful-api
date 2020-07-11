d496d4e2734582021917e985bf5833a5571b07d3
Romel Niño Paano <rpaano02@gmail.com>
2020-07-11 15:46:13 +0800

    Add buyers, categories, sellers, transactions, product and users api routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::resource('buyers','Buyer\BuyerController', ['only' => ['index', 'show']]);
+Route::resource('categories','Category\CategoryController', ['except' => ['create', 'edit']]);
+Route::resource('sellers','Seller\SellerController', ['only' => ['index', 'show']]);
+Route::resource('transactions','Transaction\TransactionController', ['only' => ['index', 'show']]);
+Route::resource('products','Product\ProductController', ['only' => ['index', 'show']]);
+Route::resource('users','User\UserController', ['except' => ['create', 'edit']]);
