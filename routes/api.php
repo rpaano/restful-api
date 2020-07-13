99d154c8ad1672db7dc6a8674c12f5d6a835ac04
Romel Niño Paano <rpaano02@gmail.com>
2020-07-13 20:31:29 +0800

    Add comments to routes in what Directory they are in

--- routes/api.php
+++ routes/api.php
@@ -17,16 +17,39 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 //});
 
+/*
+ * Buyer
+ */
 Route::resource('buyers','Buyer\BuyerController', ['only' => ['index', 'show']]);
 Route::resource('buyers.transactions','Buyer\BuyerTransactionController', ['only' => ['index']]);
 Route::resource('buyers.products','Buyer\BuyerProductController', ['only' => ['index']]);
 Route::resource('buyers.sellers','Buyer\BuyerSellerController', ['only' => ['index']]);
 Route::resource('buyers.categories','Buyer\BuyerCategoryController', ['only' => ['index']]);
+
+/*
+ * Category
+ */
 Route::resource('categories','Category\CategoryController', ['except' => ['create', 'edit']]);
 Route::resource('categories.products','Category\CategoryProductController', ['only' => ['index']]);
+
+/*
+ * Seller
+ */
 Route::resource('sellers','Seller\SellerController', ['only' => ['index', 'show']]);
+
+/*
+ * Transaction
+ */
 Route::resource('transactions','Transaction\TransactionController', ['only' => ['index', 'show']]);
 Route::resource('transactions.categories','Transaction\TransactionCategoryController', ['only' => ['index']]);
 Route::resource('transactions.sellers','Transaction\TransactionSellerController', ['only' => ['index']]);
+
+/*
+ * Product
+ */
 Route::resource('products','Product\ProductController', ['only' => ['index', 'show']]);
+
+/*
+ * User
+ */
 Route::resource('users','User\UserController', ['except' => ['create', 'edit']]);
