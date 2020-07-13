fddbf122f6328a9a018ecec75ebfe628b72a4685
Romel Niño Paano <rpaano02@gmail.com>
2020-07-13 11:50:45 +0800

    Implementing the Index Method for BuyerTransactionController

---
+++ app/Http/Controllers/Buyer/BuyerTransactionController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Buyer;
+
+use App\Buyer;
+use App\Http\Controllers\ApiController;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class BuyerTransactionController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @param Buyer $buyer
+     * @return JsonResponse
+     */
+    public function index(Buyer $buyer)
+    {
+        $transactions = $buyer->transactions;
+
+        return $this->showAll($transactions);
+    }
+}
