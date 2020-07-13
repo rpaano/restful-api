68c700a288ec04ae36c18d3972d9f2a5e0bcb065
Romel Niño Paano <rpaano02@gmail.com>
2020-07-13 18:52:54 +0800

    Add comments

--- app/Http/Controllers/Buyer/BuyerCategoryController.php
+++ app/Http/Controllers/Buyer/BuyerCategoryController.php
@@ -20,8 +20,39 @@ class BuyerCategoryController extends ApiController
             ->get()
             ->pluck('product.categories')
             ->collapse()
-            ->unique('id')
-            ->values()
+                 /*  many to many to remove the multiple array
+                        "data": {
+                                "data": [
+                                    [
+                                        {
+                                            "id": 1,
+                                            "name": "sunt",
+                                            "description": "Voluptatem voluptatem consequatur illo. Deserunt laudantium officia aut alias non.",
+                                            "deleted_at": null,
+                                            "created_at": "2020-07-12T14:21:03.000000Z",
+                                            "updated_at": "2020-07-12T14:21:03.000000Z",
+                                            "pivot": {
+                                                "product_id": 158,
+                                                "category_id": 1
+                                            }
+                                        },
+                                        {
+                                            "id": 4,
+                                            "name": "corrupti",
+                                            "description": "Eius molestiae rem dignissimos ipsa impedit excepturi. Totam eum et dolor temporibus.",
+                                            "deleted_at": null,
+                                            "created_at": "2020-07-12T14:21:03.000000Z",
+                                            "updated_at": "2020-07-12T14:21:03.000000Z",
+                                            "pivot": {
+                                                "product_id": 158,
+                                                "category_id": 4
+                                            }
+                                        }
+                                    ]
+                                 ]
+                             }    */
+            ->unique('id') //one to many to be sure there are no repeating ids
+            ->values()          //one to many to insure no to empty array
         ;
 
         return $this->showAll($categories);
--- app/Http/Controllers/Buyer/BuyerSellerController.php
+++ app/Http/Controllers/Buyer/BuyerSellerController.php
@@ -19,8 +19,8 @@ class BuyerSellerController extends ApiController
         $sellers = $buyer->transactions()->with('product.seller')
             ->get()
             ->pluck('product.seller')
-            ->unique('id')
-            ->values()
+            ->unique('id') //one to many to be sure there are no repeating ids
+            ->values()          //one to many to insure to empty array
         ;
 
         return $this->showAll($sellers);
