3c77343c2ca32745a50003f5e2cf247409d3e2ec
Romel Niño Paano <rpaano02@gmail.com>
2020-07-13 11:34:53 +0800

    Create TransactionSellerController.php

---
+++ app/Http/Controllers/Transaction/TransactionSellerController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Transaction;
+
+use App\Http\Controllers\Controller;
+use App\Transaction;
+use Illuminate\Http\Request;
+
+class TransactionSellerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+}
