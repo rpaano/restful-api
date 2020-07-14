b37fe4c2340e8ecc570233c29fb3adcc8847cf14
Romel Niño Paano <rpaano02@gmail.com>
2020-07-14 18:21:28 +0800

    Implementing the Update Operation of ProductCategory

--- app/Http/Controllers/Product/ProductCategoryController.php
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Product;
 
+use App\Category;
 use App\Http\Controllers\ApiController;
 use App\Http\Controllers\Controller;
 use App\Product;
@@ -29,22 +30,32 @@ class ProductCategoryController extends ApiController
      *
      * @param Request $request
      * @param Product $product
-     * @return Response
+     * @param Category $category
+     * @return JsonResponse
      */
-    public function update(Request $request, Product $product)
+    public function update(Request $request, Product $product, Category $category)
     {
-        //
+        /*
+         *  attach               => add to the existing connections
+         *  sync                 => remove all existing connections and attach the new one
+         *  syncWithoutDetaching => add to existing connection without duplicating
+         */
+
+        $product->categories()->syncWithoutDetaching([$category->id]);
+
+        return $this->showAll($product->categories);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param Product $product
+     * @param Category $category
      * @return JsonResponse|Response
      */
-    public function destroy(Product $product)
+    public function destroy(Product $product, Category $category)
     {
-        $product->categories()->detach();
+        $product->categories()->detach($category->id);
 
         return $this->showOne($product);
     }
