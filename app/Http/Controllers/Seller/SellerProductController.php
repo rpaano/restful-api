90bf91e95a1dc359e05267447393811004956614
Romel Niño Paano <rpaano02@gmail.com>
2020-07-15 09:08:50 +0800

    Adding an Image for Products

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -9,6 +9,7 @@ use App\User;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
@@ -49,7 +50,7 @@ class SellerProductController extends ApiController
         $data = $request->only('name', 'description', 'quantity', 'image');
 
 
-        $data['image']     = '1.png';
+        $data['image']     = $request->image->store('');
         $data['seller_id'] = $seller->id;
 
         $product = Product::create($data);
@@ -89,6 +90,12 @@ class SellerProductController extends ApiController
             }
         }
 
+        if ($request->hasFile('image')) {
+            Storage::delete($product->image);
+
+            $product->image = $request->image->store('');
+        }
+
         if ($product->isClean()){
             return $this->errorResponse('You need to specific a different value to update', 422);
         }
@@ -109,6 +116,8 @@ class SellerProductController extends ApiController
     public function destroy(Seller $seller, Product $product)
     {
         $this->checkSeller($seller, $product);
+
+//        Storage::delete($product->image);
 
         $product->delete();
 
