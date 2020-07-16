4b68e6350d9d80e266cb19605af6c22f03d3b927
Romel Niño Paano <rpaano02@gmail.com>
2020-07-16 16:57:38 +0800

    Creating the Transformer for Seller and Buyer

---
+++ app/Transformers/SellerTransformer.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Seller;
+use League\Fractal\TransformerAbstract;
+
+class SellerTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        //
+    ];
+
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $availableIncludes = [
+        //
+    ];
+
+    /**
+     * A Fractal transformer.
+     *
+     * @param Seller $seller
+     * @return array
+     */
+    public function transform(Seller $seller)
+    {
+        return [
+            'identifier' => (int)$seller->id,
+            'name' => (string)$seller->name,
+            'email' => (string)$seller->email,
+            'isVerified' => (int)$seller->verified,
+            'creationDate' => $seller->created_at,
+            'lastedDate' => $seller->updated_at,
+            'deletedDate' => isset($seller->deleted_at) ? (string)$seller->deleted_at : null,
+        ];
+    }
+}
