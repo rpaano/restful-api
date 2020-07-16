2c72fb4f8d3930505672530c9212c2218d104af3
Romel Niño Paano <rpaano02@gmail.com>
2020-07-16 17:20:59 +0800

    Creating the Transformer for Category

---
+++ app/Transformers/CategoryTransformer.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Category;
+use League\Fractal\TransformerAbstract;
+use function GuzzleHttp\Psr7\str;
+
+class CategoryTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        //
+    ];
+
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $availableIncludes = [
+        //
+    ];
+
+    /**
+     * A Fractal transformer.
+     *
+     * @param Category $category
+     * @return array
+     */
+    public function transform(Category $category)
+    {
+        return [
+            'identifier' => (int)$category->id,
+            'title' => (string)$category->name,
+            'details' => (string)$category->description,
+            'creationDate' => $category->created_at,
+            'lastedDate' => $category->updated_at,
+            'deletedDate' => isset($category->deleted_at) ? (string)$category->deleted_at : null,
+        ];
+    }
+}
