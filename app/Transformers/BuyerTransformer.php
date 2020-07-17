8f7d1fa828d06503afbde46c0ea7d93afbd3ece0
Romel Niño Paano <rpaano02@gmail.com>
2020-07-17 12:33:32 +0800

    Implementing HATEOAS for Buyer

--- app/Transformers/BuyerTransformer.php
+++ app/Transformers/BuyerTransformer.php
@@ -41,6 +41,29 @@ class BuyerTransformer extends TransformerAbstract
             'creationDate' => $buyer->created_at,
             'lastedDate' => $buyer->updated_at,
             'deletedDate' => isset($buyer->deleted_at) ? (string)$buyer->deleted_at : null,
+
+            'links' => [
+                [
+                    'rel' => 'sef',
+                    'href' => route('buyers.show', $buyer->id),
+                ],
+                [
+                    'rel' => 'buyer.categories',
+                    'href' => route('buyers.categories.index', $buyer->id),
+                ],
+                [
+                    'rel' => 'buyer.sellers',
+                    'href' => route('buyers.sellers.index', $buyer->id),
+                ],
+                [
+                    'rel' => 'buyer.transactions',
+                    'href' => route('buyers.transactions.index', $buyer->id),
+                ],
+                [
+                    'rel' => 'user',
+                    'href' => route('users.show', $buyer->id),
+                ],
+            ],
         ];
     }
 
