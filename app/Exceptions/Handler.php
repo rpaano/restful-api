750943bb79116d922fd7a69fab6ae822e1109e80
Romel Niño Paano <rpaano02@gmail.com>
2020-07-18 10:49:13 +0800

    About CSRF and Handling CSRF Exceptions in Laravel

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -8,6 +8,7 @@ use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Illuminate\Validation\ValidationException;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
@@ -97,6 +98,10 @@ class Handler extends ExceptionHandler
             if ($errorCode == 1451){
                 return $this->errorResponse('Cannot remove this remove resource permanently. It is related with other resources.', 409);
             }
+        }
+
+        if ($exception instanceof TokenMismatchException){
+            return redirect()->back()->withInput($request->input());
         }
 
         if (config("app.debug") == true){
